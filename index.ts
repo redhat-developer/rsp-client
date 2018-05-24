5d95d919ba8f1218d47fbedf4647cf14b5abf491
Sudhir Verma <sudhirvvermaa@gmail.com>
2018-05-24 17:47:22 +0530

    Added hello world example

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
-import * as pluralize from 'pluralize'
-/**
-* @Method: Returns the plural form of any noun.
-* @Param {string}
-* @Return {string}
-*/
-export function getPlural (str: any) : string {
-  return pluralize.plural(str)
+
+export function helloworld(str: string): string {
+
+  return "hello world";
 }
