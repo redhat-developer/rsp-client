7bea34bbd57fcf6c81a0523b0b1232e361fd0109
Rob Stryker <rob@oxbeef.net>
2019-01-10 15:55:33 -0500

    Remove duplicate field
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/protocol/protocol.ts
+++ src/protocol/protocol.ts
@@ -155,7 +155,6 @@ export namespace Protocol {
 
     export interface Status {
         severity: number;
-        pluginId: string;
         code: number;
         message: string;
         trace: string;
