03c1fd00db7ce9b10389b1fbd97bbccc7e17c8bc
Andre Dietisheim <adietish@redhat.com>
2019-03-05 14:14:55 +0100

    provided StatusSeverity#isOk, #isError etc. to avoid numerical testing of it
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

---
+++ src/util/statusSeverity.ts
@@ -0,0 +1,30 @@
+import { Protocol } from '../protocol/protocol';
+
+export class StatusSeverity {
+
+    public static isOk(status: Protocol.Status): boolean {
+        return status
+          && status.severity === 0;
+    }
+
+    public static isInfo(status: Protocol.Status): boolean {
+        return StatusSeverity.testSeverity(1, status);
+    }
+
+    public static isWarning(status: Protocol.Status): boolean {
+        return StatusSeverity.testSeverity(2, status);
+    }
+
+    public static isError(status: Protocol.Status): boolean {
+        return StatusSeverity.testSeverity(4, status);
+    }
+
+    public static isCancel(status: Protocol.Status): boolean {
+        return StatusSeverity.testSeverity(8, status);
+    }
+
+    private static testSeverity(bitmask: number, status: Protocol.Status): boolean {
+        return status
+          && (status.severity & bitmask) === bitmask;
+    }
+}
