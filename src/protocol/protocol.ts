ed75d8fcd60c3e1009eebe459b542ba90f280930
Rob Stryker <rob@oxbeef.net>
2019-03-14 22:52:08 -0400

    Update for job API, auto-generated more files
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/protocol/protocol.ts
+++ src/protocol/protocol.ts
@@ -2,50 +2,52 @@
  * Json objects sent between the server and the client
  */
 export namespace Protocol {
-
+    /* tslint:disable */
+    // Generated using typescript-generator version 2.2.413 on 2019-03-14 22:50:48.
+    
     export interface Attribute {
         type: string;
         description: string;
         defaultVal: any;
         secret: boolean;
     }
-
+    
     export interface Attributes {
         attributes: { [index: string]: Attribute };
     }
-
+    
     export interface ClientCapabilitiesRequest {
         map: { [index: string]: string };
     }
-
+    
     export interface CommandLineDetails {
         cmdLine: string[];
         workingDir: string;
         envp: string[];
         properties: { [index: string]: string };
     }
-
+    
     export interface CreateServerResponse {
         status: Status;
         invalidKeys: string[];
     }
-
+    
     export interface DeployableReference {
         label: string;
         path: string;
     }
-
+    
     export interface DeployableState {
         server: ServerHandle;
         reference: DeployableReference;
         state: number;
         publishState: number;
     }
-
+    
     export interface DiscoveryPath {
         filepath: string;
     }
-
+    
     export interface DownloadRuntimeDescription {
         name: string;
         id: string;
@@ -58,43 +60,58 @@ export namespace Protocol {
         size: string;
         installationMethod: string;
     }
-
+    
     export interface DownloadSingleRuntimeRequest {
         requestId: number;
         downloadRuntimeId: string;
         data: { [index: string]: any };
     }
-
+    
+    export interface JobHandle {
+        name: string;
+        id: string;
+    }
+    
+    export interface JobProgress {
+        percent: number;
+        handle: JobHandle;
+    }
+    
+    export interface JobRemoved {
+        status: Status;
+        handle: JobHandle;
+    }
+    
     export interface LaunchAttributesRequest {
         serverTypeId: string;
         mode: string;
     }
-
+    
     export interface LaunchParameters {
         mode: string;
         params: ServerAttributes;
     }
-
+    
     export interface ListDownloadRuntimeResponse {
         runtimes: DownloadRuntimeDescription[];
     }
-
+    
     export interface ModifyDeployableRequest {
         server: ServerHandle;
         deployable: DeployableReference;
     }
-
+    
     export interface PublishServerRequest {
         server: ServerHandle;
         kind: number;
     }
-
+    
     export interface ServerAttributes {
         serverType: string;
         id: string;
         attributes: { [index: string]: any };
     }
-
+    
     export interface ServerBean {
         location: string;
         typeCategory: string;
@@ -104,57 +121,57 @@ export namespace Protocol {
         fullVersion: string;
         serverAdapterTypeId: string;
     }
-
+    
     export interface ServerCapabilitiesResponse {
         serverCapabilities: { [index: string]: string };
         clientRegistrationStatus: Status;
     }
-
+    
     export interface ServerHandle {
         id: string;
         type: ServerType;
     }
-
+    
     export interface ServerLaunchMode {
         mode: string;
         desc: string;
     }
-
+    
     export interface ServerProcess {
         server: ServerHandle;
         processId: string;
     }
-
+    
     export interface ServerProcessOutput {
         server: ServerHandle;
         processId: string;
         streamType: number;
         text: string;
     }
-
+    
     export interface ServerStartingAttributes {
         initiatePolling: boolean;
         request: LaunchParameters;
     }
-
+    
     export interface ServerState {
         server: ServerHandle;
         state: number;
         publishState: number;
         deployableStates: DeployableState[];
     }
-
+    
     export interface ServerType {
         id: string;
         visibleName: string;
         description: string;
     }
-
+    
     export interface StartServerResponse {
         status: Status;
         details: CommandLineDetails;
     }
-
+    
     export interface Status {
         severity: number;
         plugin: string;
@@ -163,34 +180,35 @@ export namespace Protocol {
         trace: string;
         ok: boolean;
     }
-
+    
     export interface StopServerAttributes {
         id: string;
         force: boolean;
     }
-
+    
     export interface StringPrompt {
         code: number;
         prompt: string;
         secret: boolean;
     }
-
+    
     export interface VMDescription {
         id: string;
         installLocation: string;
         version: string;
     }
-
+    
     export interface VMHandle {
         id: string;
     }
-
+    
     export interface WorkflowResponse {
         status: Status;
         requestId: number;
+        jobId: string;
         items: WorkflowResponseItem[];
     }
-
+    
     export interface WorkflowResponseItem {
         id: string;
         itemType: string;
@@ -200,4 +218,4 @@ export namespace Protocol {
         responseSecret: boolean;
         validResponses: string[];
     }
-}
+    }
